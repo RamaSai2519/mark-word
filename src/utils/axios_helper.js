b6784e562ed72546550e21a50ec4130229637674
RamaSai2519 <rchandik@gitam.in>
2025-02-27 17:50:01 +0530

    chore: Update axios base URL and add launch configuration for Mark Word

--- src/utils/axios_helper.js
+++ src/utils/axios_helper.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const raxios = axios.create({
     // baseURL: 'http://localhost:8000'
-    baseURL: 'https://test.sukoonunlimited.com'
+    baseURL: 'https://mword.sukoonunlimited.com'
 });
 
 export default raxios;
